dceab72c96d71d337085b2f41337541b905296e7
maxday <maxime.david@datadoghq.com>
2022-09-15 17:25:40 -0400

    fix tests

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,8 @@ pub struct AuthenticationContext {
     access_key_id: String,
     secret_access_key: String,
     session_token: String,
-    current_time: SystemTime,
+    date: String,
+    date_time: String,
 }
 
 impl AuthenticationContext {
@@ -33,13 +34,29 @@ impl AuthenticationContext {
         let access_key_id = std::env::var(ENV_ACCESS_KEY).expect("Could not find AWS_ACCESS_KEY_ID value");
         let secret_access_key = std::env::var(ENV_SECRET_KEY).expect("Could not find AWS_SECRET_ACCESS_KEY value");
         let session_token = std::env::var(ENV_SESSION_TOKEN).expect("Could not find AWS_SESSION_TOKEN value");
+        let current_time = SystemTime::now();
         AuthenticationContext { 
             region,
             function_name,
             access_key_id,
             secret_access_key,
             session_token,
-            current_time: SystemTime::now()
+            date: format_date(current_time),
+            date_time: format_date_time(current_time),
+        }
+    }
+
+    #[cfg(test)]
+    fn with_time(date: &str, date_time: &str) -> AuthenticationContext {
+        let context = AuthenticationContext::new();
+        AuthenticationContext {
+            region: context.region,
+            function_name: context.function_name,
+            access_key_id: context.access_key_id,
+            secret_access_key: context.secret_access_key,
+            session_token: context.session_token,
+            date: date.to_string(),
+            date_time: date_time.to_string(),
         }
     }
 }
@@ -52,40 +69,47 @@ fn main() {
 pub fn decrypt(cipher_blob: &str, full_json: bool) -> Result<String> {
     let auth_context = AuthenticationContext::new();
     let function_name = auth_context.function_name.clone();
-    let payload = build_payload(cipher_blob, function_name);
+    let payload = build_payload(cipher_blob, function_name.as_str());
     let api_result = send(&auth_context, payload.as_str())?;
     return Ok(format_result(&api_result, full_json))
 }
 
-pub fn build_payload(cipher_blob: &str, function_name: String) -> String {
+//tested
+pub fn build_payload(cipher_blob: &str, function_name: &str) -> String {
     return format!("{{\"CiphertextBlob\":\"{}\",\"EncryptionContext\":{{\"LambdaFunctionName\":\"{}\"}}}}", cipher_blob, function_name);
 } 
 
+//tested
 pub fn build_header(auth_context: &AuthenticationContext) -> String {
-    format!("{}/{}/kms/aws4_request", format_date(auth_context.current_time), auth_context.region).to_string()
+    format!("{}/{}/kms/aws4_request", auth_context.date, auth_context.region)
 }
 
+//tested
 pub fn build_signature(data: &str, header: String, auth_context: &AuthenticationContext) -> String {
     let creds = derive_signing_key(auth_context);
     let canonical_string = build_canonical_string(auth_context, data);
     let reader = BufReader::new(canonical_string.as_bytes());
     let digest = sha256_digest(reader).unwrap();
+    let hex_digest = hex::encode(digest);
+
     let string_to_sign = format!(
         "{}\n{}\n{}\n{}",
         AUTH_HEDER_PREFIX,
-        format_date_time(auth_context.current_time),
+        auth_context.date_time,
         header,
-        pretty_sha256(digest)
+        hex_digest
     );
     let key = hmac::Key::new(hmac::HMAC_SHA256, creds.as_ref());
     let signature = hmac::sign(&key, string_to_sign.as_bytes());
-    pretty_tag(signature)
+    return hex::encode(signature);
 }
 
+// tested
 pub fn build_authorization_token(auth_context: &AuthenticationContext, signature: String) -> String {
-    return format!("AWS4-HMAC-SHA256 Credential={}/{}/{}/kms/aws4_request, SignedHeaders=content-length;content-type;host;x-amz-date;x-amz-security-token;x-amz-target, Signature={}", auth_context.access_key_id, format_date(auth_context.current_time), auth_context.region, signature);
+    return format!("AWS4-HMAC-SHA256 Credential={}/{}/{}/kms/aws4_request, SignedHeaders=content-length;content-type;host;x-amz-date;x-amz-security-token;x-amz-target, Signature={}", auth_context.access_key_id, auth_context.date, auth_context.region, signature);
 }
 
+// tested
 pub fn pretty_tag(tag: Tag) -> String {
     let pretty_format = format!("{:?}", tag);
     pretty_format[11..pretty_format.len() - 1].to_string()
@@ -99,7 +123,7 @@ pub fn pretty_sha256(digest: Digest) -> String {
 pub fn build_canonical_string(auth_context: &AuthenticationContext, body: &str) -> String {
     let reader = BufReader::new(body.as_bytes());
     let digest = sha256_digest(reader).unwrap();
-    let canonical_headers = format!("content-length:{}\ncontent-type:application/x-amz-json-1.1\nhost:kms.{}.amazonaws.com\nx-amz-date:{}\nx-amz-security-token:{}\nx-amz-target:TrentService.Decrypt\n", body.len(), auth_context.region, format_date_time(auth_context.current_time),auth_context.session_token);
+    let canonical_headers = format!("content-length:{}\ncontent-type:application/x-amz-json-1.1\nhost:kms.{}.amazonaws.com\nx-amz-date:{}\nx-amz-security-token:{}\nx-amz-target:TrentService.Decrypt\n", body.len(), auth_context.region,auth_context.date_time,auth_context.session_token);
     let return_string = format!(
         "POST\n/\n\n{}\ncontent-length;content-type;host;x-amz-date;x-amz-security-token;x-amz-target\n{}",
         canonical_headers,
@@ -125,7 +149,7 @@ fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest> {
 pub fn derive_signing_key(auth_context: &AuthenticationContext) -> hmac::Tag {
     let secret = format!("AWS4{}", auth_context.secret_access_key);
     let key = hmac::Key::new(hmac::HMAC_SHA256, secret.as_bytes());
-    let date_tag = hmac::sign(&key, format_date(auth_context.current_time).as_bytes());
+    let date_tag = hmac::sign(&key, auth_context.date.as_bytes());
     let key = hmac::Key::new(hmac::HMAC_SHA256, date_tag.as_ref());
     let region_tag = hmac::sign(&key, auth_context.region.as_bytes());
     let key = hmac::Key::new(hmac::HMAC_SHA256, region_tag.as_ref());
@@ -169,7 +193,7 @@ fn build_header_list(auth_context: &AuthenticationContext, payload: &str) -> Res
     list.append("X-Amz-Target: TrentService.Decrypt")?;
     list.append("Content-Type: application/x-amz-json-1.1")?;
     list.append(format!("Content-Length: {}", content_length).as_str())?;
-    list.append(format!("X-Amz-Date: {}", format_date_time(auth_context.current_time)).as_str())?;
+    list.append(format!("X-Amz-Date: {}", auth_context.date_time).as_str())?;
     list.append(format!("Authorization: {}", authorization_token).as_str())?;
     list.append(format!("X-Amz-Security-Token: {}", auth_context.session_token).as_str())?;
     Ok(list)
@@ -213,29 +237,63 @@ pub fn format_result(json_response: &[u8], full_json: bool) -> String {
 
 
 
-// use std::io::Read;
-// use curl::easy::Easy;
+#[cfg(test)]
+mod tests {
 
-// fn main() {
-//     let mut data = "this is the body".as_bytes();
-//     let mut dst = Vec::new();
-//     {
-//         let mut easy = Easy::new();
-//         easy.url("http://www.example.com/upload").unwrap();
-//         easy.post(true).unwrap();
-//         easy.post_field_size(data.len() as u64).unwrap();
+    use super::*;
+ 
+    #[test]
+    fn test_build_payload() {
+        let cipher_blob = "abc";
+        let function_name = "my_function";
+        assert_eq!("{\"CiphertextBlob\":\"abc\",\"EncryptionContext\":{\"LambdaFunctionName\":\"my_function\"}}", build_payload(cipher_blob, function_name));
+    }
 
-//         let mut transfer = easy.transfer();
-//         transfer.read_function(|buf| {
-//             Ok(data.read(buf).unwrap_or(0))
-//         }).unwrap();
+    #[test]
+    fn test_build_header() {
+        set_test_env();
+        let auth_context = AuthenticationContext::with_time("20220101", "20220101T010203Z");
+        let result = build_header(&auth_context);
+        let expected = "20220101/test-region/kms/aws4_request";
+        unset_test_env();
+        assert_eq!(expected, result);
+    }
 
-//         transfer.write_function(|data| {
-//             dst.extend_from_slice(data);
-//             Ok(data.len())
-//         }).unwrap();
+    #[test]
+    fn test_build_authorization_token() {
+        set_test_env();
+        let auth_context = AuthenticationContext::with_time("20220101", "20220101T010203Z");
+        println!("{:?}", auth_context.date_time);
+        let result = build_authorization_token(&auth_context, "test-signature".to_string());
+        unset_test_env();
+        let expected = "AWS4-HMAC-SHA256 Credential=test-access-key/20220101/test-region/kms/aws4_request, SignedHeaders=content-length;content-type;host;x-amz-date;x-amz-security-token;x-amz-target, Signature=test-signature";
+        assert_eq!(expected, result);
+    }
 
-//         transfer.perform().unwrap();
-//     }
-//     println!("{:?}", dst);
-// }
+    #[test]
+    fn test_build_signature() {
+        set_test_env();
+        let auth_context = AuthenticationContext::with_time("20220101", "20220101T010203Z");
+        let headers = "20220101/test-region/kms/aws4_request".to_string();
+        let result = build_signature("test-data", headers, &auth_context);
+        unset_test_env();
+        assert_eq!("2d00a717685bf2e0b2b3b70f8718d0b9cbe28875da76cb10600b06e88f213b7a", result);
+    }
+
+    fn set_test_env() {
+        std::env::set_var(ENV_REGION, "test-region");
+        std::env::set_var(ENV_FUNCTION_NAME, "test-function-name");
+        std::env::set_var(ENV_ACCESS_KEY, "test-access-key");
+        std::env::set_var(ENV_SECRET_KEY, "test-secret-key");
+        std::env::set_var(ENV_SESSION_TOKEN, "test-session-token");
+    }
+
+    fn unset_test_env() {
+        std::env::remove_var(ENV_REGION);
+        std::env::remove_var(ENV_FUNCTION_NAME);
+        std::env::remove_var(ENV_ACCESS_KEY);
+        std::env::remove_var(ENV_SECRET_KEY);
+        std::env::remove_var(ENV_SESSION_TOKEN);
+
+    }
+}
